50567ebdb75bb22b9179bda57de55e2de663836b
LopezAlCuadrado8 <daniellopezalcuadrado@gmail.com>
2024-10-14 19:01:49 -0500

    Cambios en el Main Menu

--- Assets/Scripts/Main Menu/Changing Image.cs
+++ Assets/Scripts/Main Menu/Changing Image.cs
@@ -1,16 +1,26 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ChangingImage : MonoBehaviour
 {
+    [Header("Propiedades")]
+    [SerializeField]
+    private float changeImageDelay = 1.5f;
+
+    [Header("Imagenes")]
     [SerializeField]
     private List<Sprite> Sprites;
     
 
-    private Sprite sprite;
+    private Image image;
     void Start(){
-        
+        image = GetComponent<Image>();
+    }
+
+
+    public void ChangeImageMethod(){
+        int randomImagePositon = Random.Range(0, Sprites.Count);
+        image.sprite = Sprites[randomImagePositon];
     }
 }
